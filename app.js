9f5fda67e3c0b2c30ec40fc1375733e54a1d40d4
陳德元 <r06521608@ntu.edu.tw>
2021-01-12 14:02:20 -0600

    add folder

--- app.js
+++ app.js
@@ -1,57 +1,19 @@
-// const data = [10, 20, 30, 40, 50]
+async function draw() {
+	const dataset = await d3.json('data.json')
 
-// const el = d3
-//   .select('ul')
-//   .selectAll('li')
-//   .data(data)
-//   .join('li') //to match the exact number of the data
-//   .text(d => d) //display data!
+    let dimensions = {
+    	width: 800,
+    	height: 800
+    }
 
-// const el = d3
-//   .select('ul')
-//   .selectAll('li')
-//   .data(data)
-//   .join(
-//     enter => {
-//     	return enter.append('li')
-//     	  .style('color', 'purple')
-//     },
-//     update => update.style('color', 'green'),
-//     exit => exit.remove()
-//   ) 
-//   .text(d => d) //display data!
+    // Draw Image
+    const svg = d3.select('#chart')
+      .append('svg')
+      .attr('width', dimensions.width)
+      .attr('height', dimensions.height)
 
-// const el = d3
-//   .select('ul')
-//   .selectAll('li')
-//   .data(data)
-//   .text(d => d) //display data!
-
-// el.enter()
-//   .append('li')
-//   .text(d => d)
-
-// el.exit().remove()  
-
-// console.log(el);
-
-// const pD3 = d3.select('p')
-
-// d3.json('data.json').then((data) => {
-// 	console.log(data)
-// })
-
-async function getData(){
-	const data = await d3.json('data.json')
-	// const data = await d3.csv('data.csv')
-	console.log(data)
+    svg.append('circle')
+      .attr('r', 15)
 }
-getData()
 
-
-
-
-
-
-
-
+draw()
