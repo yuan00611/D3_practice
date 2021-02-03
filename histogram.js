a662a19fc87dc1020419df8421f36c1a849d0cdf
陳德元 <r06521608@ntu.edu.tw>
2021-02-03 16:57:23 -0600

    can change histogram by different options

--- histogram.js
+++ histogram.js
@@ -2,9 +2,7 @@ async function draw() {
     //Data
     const dataset = await d3.json('scatterPlotData.json');
 
-    const xAccessor = d => d.currently.humidity;
-    const yAccessor = d => d.length;
-
+    
     //Dimensions
     let dimensions = {
         width: 800,
@@ -24,59 +22,65 @@ async function draw() {
     const ctr = svg.append('g')
         .attr('transform', `translate(${dimensions.margin}, ${dimensions.margin})`)
     
-    //Scale
-    const xScale = d3.scaleLinear()
-        .domain(d3.extent(dataset, xAccessor))
-        .range([0, dimensions.ctrWidth])
-        .nice()
-
-    // value to frequency
-    const bin = d3.bin()
-        .domain(xScale.domain())
-        .value(xAccessor)
-        .thresholds(10)
-
-    const newDataset = bin(dataset)
-    const pandding = 1
-
-    const yScale = d3.scaleLinear()
-        .domain([0, d3.max(newDataset, yAccessor)])
-        .range([dimensions.ctrHeight, 0])
-        .nice()
-
-    // console.log({ original: dataset, new: newDataset});
     
-    //Draw Bars
-    ctr.selectAll('rect')
-        .data(newDataset)
-        .join('rect')
-        .attr('width', d => d3.max([0, xScale(d.x1) - xScale(d.x0) - pandding]))
-        .attr('height', d => dimensions.ctrHeight - yScale(yAccessor(d)))
-        .attr('x', d => xScale(d.x0))
-        .attr('y', d => yScale(yAccessor(d)))
-        .attr('fill', '#01c5c4')
-
-    ctr.append('g')
+    const labelsGroup = ctr.append('g')
         .classed('bar-labels', true)
-        .selectAll('text')
-        .data(newDataset)
-        .join('text')
-        .attr('x', d => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
-        .attr('y', d => yScale(yAccessor(d)) - 10)
-        .text(yAccessor)
-
-    //Draw Axis
-    const xAxis = d3.axisBottom(xScale)
 
     const xAxisGroup = ctr.append('g')
         .style('transform', `translateY(${dimensions.ctrHeight}px)`)
 
-    xAxisGroup.call(xAxis)
 
+    
     function histogram(metric) {
+        const xAccessor = d => d.currently[metric];
+        const yAccessor = d => d.length;
+
+        //Scale
+        const xScale = d3.scaleLinear()
+            .domain(d3.extent(dataset, xAccessor))
+            .range([0, dimensions.ctrWidth])
+            .nice()
+
+        // value to frequency
+        const bin = d3.bin()
+            .domain(xScale.domain())
+            .value(xAccessor)
+            .thresholds(10)
+
+        const newDataset = bin(dataset)
+        const pandding = 1
+
+        const yScale = d3.scaleLinear()
+            .domain([0, d3.max(newDataset, yAccessor)])
+            .range([dimensions.ctrHeight, 0])
+            .nice()
+
+        // console.log({ original: dataset, new: newDataset});
+
+        //Draw Bars
+        ctr.selectAll('rect')
+            .data(newDataset)
+            .join('rect')
+            .attr('width', d => d3.max([0, xScale(d.x1) - xScale(d.x0) - pandding]))
+            .attr('height', d => dimensions.ctrHeight - yScale(yAccessor(d)))
+            .attr('x', d => xScale(d.x0))
+            .attr('y', d => yScale(yAccessor(d)))
+            .attr('fill', '#01c5c4')
+
+        labelsGroup.selectAll('text')
+            .data(newDataset)
+            .join('text')
+            .attr('x', d => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
+            .attr('y', d => yScale(yAccessor(d)) - 10)
+            .text(yAccessor)
+
+        //Draw Axis
+        const xAxis = d3.axisBottom(xScale)
+
+        xAxisGroup.call(xAxis)
+
 
     }
-
 
     //Event
     d3.select("#metric").on('change', function (e) {
@@ -85,6 +89,8 @@ async function draw() {
         histogram(this.value)
 
     })
+
+    histogram('humidity')
 
 
 }
